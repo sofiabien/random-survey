1ad125792ad1110c88e3e62a93a5ff19e54b3e15
sofiabien <165005127+sofiabien@users.noreply.github.com>
2024-04-02 01:21:29 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,5 +9,11 @@
 
 const urls = [
 	'https://forms.fillout.com/t/ruzFt7t88uus',
-	'https://forms.fillout.com/t/bNkzFC6Y5sus'
+	'https://forms.fillout.com/t/bNkzFC6Y5sus',
+	'https://forms.fillout.com/t/u2z6w6DqCBus',
+	'https://forms.fillout.com/t/bbWefinfipus',
+	'https://forms.fillout.com/t/svBCLTCsTzus',
+	'https://forms.fillout.com/t/8gc7wbZrSnus',
+	'https://forms.fillout.com/t/sxUc5Rt2yWus',
+	'https://forms.fillout.com/t/bBvCFqxUh1us'
 ];
